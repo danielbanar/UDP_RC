c3464a378d45ed219b818e1a983d640aeb35d766
Daniel Banár <daniel.banar5@gmail.com>
2024-06-08 22:48:29 +0200

    simple payload order check + autocorrect

--- client.cpp
+++ client.cpp
@@ -17,82 +17,99 @@
 #define MAP_RANGE(value, fromLow, fromHigh, toLow, toHigh) ((value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow)
 
 void receiveMessages(int sockfd, struct sockaddr_in& serverAddr) {
-    char buffer[BUFFER_SIZE];
-    socklen_t addrLen = sizeof(serverAddr);
-    std::regex regexPattern("N(-?\\d+)LX(-?\\d+)LT(-?\\d+)RT(-?\\d+)\\n");
+	char buffer[BUFFER_SIZE];
+	socklen_t addrLen = sizeof(serverAddr);
+	std::regex regexPattern("N(-?\\d+)LX(-?\\d+)LT(-?\\d+)RT(-?\\d+)\\n");
+	
+	while (true) 
+	{
+		int len = recvfrom(sockfd, buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr*)&serverAddr, &addrLen);
+		buffer[len] = '\0';
 
-    while (true) {
-        int len = recvfrom(sockfd, buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr*)&serverAddr, &addrLen);
-        buffer[len] = '\0';
-
-        std::cmatch matches;
-        if (std::regex_search(buffer, matches, regexPattern)) 
-        {
-            int N = std::stoi(matches[1]);
-            int LX = std::stoi(matches[2]);
-            int LT = std::stoi(matches[3]);
-            int RT = std::stoi(matches[4]);
-            gpioServo(SERVO_PIN, MAP_RANGE(LX,-32768, 32767, 1000, 2000));
-            gpioServo(SERVO_PIN, MAP_RANGE(RT-LT,-255, 255, 1000, 2000));
-        }
-        else 
-        {
-            std::cout << "Received: " << buffer;
-        }
-    }
+		std::cmatch matches;
+		if (std::regex_search(buffer, matches, regexPattern))
+		{
+			static auto lastValidPayload = std::chrono::high_resolution_clock::now();
+			static int lastN = 0;
+			int N = std::stoi(matches[1]);
+			if (lastN < N)
+			{
+				int LX = std::stoi(matches[2]);
+				int LT = std::stoi(matches[3]);
+				int RT = std::stoi(matches[4]);
+				gpioServo(SERVO_PIN, MAP_RANGE(LX, -32768, 32767, 1000, 2000));
+				gpioServo(THROTTLE_PIN, MAP_RANGE(RT - LT, -255, 255, 1000, 2000));
+				//std::cout << "T" << MAP_RANGE(LX, -32768, 32767, 1000, 2000) << std::endl;
+				//std::cout << "S" << MAP_RANGE(RT - LT, -255, 255, 1000, 2000) << std::endl;
+				lastN = N;
+				lastValidPayload = std::chrono::high_resolution_clock::now();
+			}
+			else
+			{
+				auto currentTime = std::chrono::high_resolution_clock::now();
+				auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - lastValidPayload).count();
+				if (elapsedTime >= 2)
+					lastN = 0;
+			}
+		}
+		else
+		{
+			std::cout << "Received: " << buffer;
+		}
+	}
 }
 
 int main() {
-    int sockfd;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in serverAddr;
-    struct addrinfo hints, * res;
+	int sockfd;
+	char buffer[BUFFER_SIZE];
+	struct sockaddr_in serverAddr;
+	struct addrinfo hints, * res;
 
-    // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+	// Creating socket file descriptor
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("Socket creation failed");
+		exit(EXIT_FAILURE);
+	}
 
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    memset(&hints, 0, sizeof(hints));
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	memset(&hints, 0, sizeof(hints));
 
-    hints.ai_family = AF_INET; // IPv4
-    hints.ai_socktype = SOCK_DGRAM; // UDP
+	hints.ai_family = AF_INET; // IPv4
+	hints.ai_socktype = SOCK_DGRAM; // UDP
 
-    // Resolve the hostname to an IP address
-    const char* hostname = "tutos.ddns.net";
-    if (getaddrinfo(hostname, NULL, &hints, &res) != 0) {
-        perror("getaddrinfo failed");
-        exit(EXIT_FAILURE);
-    }
+	// Resolve the hostname to an IP address
+	const char* hostname = "tutos.ddns.net";
+	if (getaddrinfo(hostname, NULL, &hints, &res) != 0) {
+		perror("getaddrinfo failed");
+		exit(EXIT_FAILURE);
+	}
 
-    // Copy the resolved address to serverAddr
-    serverAddr = *(struct sockaddr_in*)(res->ai_addr);
-    serverAddr.sin_port = htons(PORT);
+	// Copy the resolved address to serverAddr
+	serverAddr = *(struct sockaddr_in*)(res->ai_addr);
+	serverAddr.sin_port = htons(PORT);
 
-    freeaddrinfo(res);
+	freeaddrinfo(res);
 
-    if (gpioInitialise() < 0) {
-        std::cerr << "Failed to initialize pigpio" << std::endl;
-        return 1;
-    }
+	if (gpioInitialise() < 0) {
+		std::cerr << "Failed to initialize pigpio" << std::endl;
+		return 1;
+	}
 
-    // Start a thread to receive messages from the server
-    std::thread receiveThread(receiveMessages, sockfd, std::ref(serverAddr));
+	// Start a thread to receive messages from the server
+	std::thread receiveThread(receiveMessages, sockfd, std::ref(serverAddr));
 
-    while (true) {
-        const char* message = "Hello from client";
-        sendto(sockfd, message, strlen(message), MSG_CONFIRM, (const struct sockaddr*)&serverAddr, sizeof(serverAddr));
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+	while (true) {
+		const char* message = "Hello from client";
+		sendto(sockfd, message, strlen(message), MSG_CONFIRM, (const struct sockaddr*)&serverAddr, sizeof(serverAddr));
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
 
-    receiveThread.join();
-    close(sockfd);
-    // Stop servo pulses
-    gpioServo(SERVO_PIN, 0);
+	receiveThread.join();
+	close(sockfd);
+	// Stop servo pulses
+	gpioServo(SERVO_PIN, 0);
 
-    // Terminate pigpio library
-    gpioTerminate();
-    return 0;
+	// Terminate pigpio library
+	gpioTerminate();
+	return 0;
 }
